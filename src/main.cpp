9f1e864fe8f9cbb2e1b0f40c9949adf41bf3c3c7
Chance Phiniezy <phiniezyc@gmail.com>
2019-07-17 22:40:07 -0400

    Add variable

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,9 @@
 
 int main() {
   int favoriteNumber;
-  std::cout << "Enter Number between 1 and 100" <<std::endl;
+  std::cout << "Enter Number between 1 and 100: " <<std::endl;
   std::cin >> favoriteNumber;
-  std::cout <<favoriteNumber;
+  std::cout << "That's my favorite number!" <<std::endl;
+  std::cout << "No really " <<favoriteNumber << " is my favorite number!" ;
   return 0;
 }
