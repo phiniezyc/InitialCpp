1a40ed001111132f803797c361275a48db3f2318
Chance Phiniezy <phiniezyc@gmail.com>
2019-07-20 00:52:08 -0400

    Add working prompt

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,21 @@
 #include <iostream>
 
 
-using namespace std;
+using namespace std; // used so don't have to type std::cout etc...
 int main() {
   int favoriteNumber;
-  cout << "Enter Number between 1 and 100: " <<std::endl;
+  cout << "Enter Number between 1 and 100: " <<endl;
   cin >> favoriteNumber;
-  cout << "That's my favorite number!" <<std::endl;
-  cout << "No really " <<favoriteNumber << " is my favorite number!" ;
-  
+  cout << "That's my favorite number!" <<endl;
+  cout << "No really " <<favoriteNumber << " is my favorite number!" <<endl;
+  cout <<endl;
+
+  int pizzas {0};
+  const double price {12.5};
+
+  cout << "Bonjour !  T'en veux combien ?" <<endl;
+  cin >> pizzas;
+  cout <<"C'est " <<price*pizzas <<" Euros.";
+
   return 0;
 }
